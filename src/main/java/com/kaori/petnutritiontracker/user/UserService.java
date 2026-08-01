dbe5ce590e1ce0a2a2d8d4ba172273e1a13e5efe
John Doe <arina.soldan@gmail.com>
2026-08-01 09:13:36 +0300

    feat: add user service

---
+++ src/main/java/com/kaori/petnutritiontracker/user/UserService.java
@@ -0,0 +1,30 @@
+package com.kaori.petnutritiontracker.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+}
