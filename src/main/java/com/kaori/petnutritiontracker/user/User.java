0578a53d22a988cd76843c2ff2a9b03e7b46116b
John Doe <arina.soldan@gmail.com>
2026-08-03 23:39:16 +0300

    refactor: add JPA constraints

--- src/main/java/com/kaori/petnutritiontracker/user/User.java
+++ src/main/java/com/kaori/petnutritiontracker/user/User.java
@@ -18,12 +18,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
 }
